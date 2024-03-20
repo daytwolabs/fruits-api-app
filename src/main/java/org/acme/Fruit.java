344e0fc07ef264bf12d9b38ee826a59a9a6daa9b
Ricardo Arguello <ricardo.arguello@gmail.com>
2024-03-19 23:39:39 -0500

    Initial version

---
+++ src/main/java/org/acme/Fruit.java
@@ -0,0 +1,17 @@
+package org.acme;
+import io.quarkus.runtime.annotations.RegisterForReflection;
+
+@RegisterForReflection
+public class Fruit {
+
+    public String name;
+    public String description;
+
+    public Fruit() {
+    }
+
+    public Fruit(String name, String description) {
+        this.name = name;
+        this.description = description;
+    }
+}
